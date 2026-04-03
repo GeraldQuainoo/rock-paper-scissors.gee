a82aecfdfd084f2b7027e4d73f58379af232e219
GeraldQuainoo <150604676+GeraldQuainoo@users.noreply.github.com>
2026-04-03 00:33:57 +0000

    Add files via upload

---
+++ rock-paper-scissors/index.js
@@ -0,0 +1,241 @@
+const container = document.querySelector(".container");
+const userScoreNumber = document.querySelector(".user-score-number");
+const computerScoreNumber = document.querySelector(".computer-score-number");
+const computerBoard = document.querySelector(".computer-board");
+const winner = document.querySelector(".score-board");
+const score = document.querySelector(".score");
+const choices = document.querySelector(".choices");
+const btns = document.querySelectorAll(".choice-btn");
+const result = document.querySelector(".result");
+const resetBtn = document.querySelector(".reset-btn");
+const computerChoice = document.querySelector(".computer-option");
+const ready = document.querySelector(".ready");
+const go = document.querySelector(".go");
+const emoji = document.querySelector(".emoji");
+const day = document.querySelector(".day");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+const body = document.querySelector("body");
+
+moon.addEventListener("click", function () {
+  sun.classList.remove("hidden");
+  moon.classList.add("hidden");
+  body.classList.toggle("dark");
+});
+sun.addEventListener("click", function () {
+  sun.classList.add("hidden");
+  moon.classList.remove("hidden");
+  body.classList.toggle("dark");
+});
+const state = {
+  isPlaying: true,
+  playerScore: 0,
+  computerScore: 0,
+};
+
+const options = { 1: "🪨", 2: "📄", 3: "✂️" };
+const letsGo = ["Rock", "Paper", "Scissors"];
+let userAns = [];
+let machineAns = [];
+let machineChoice;
+let ans;
+computerChoice.textContent = "";
+userScoreNumber.textContent = 0;
+computerScoreNumber.textContent = 0;
+
+const init = function () {
+  userAns = [];
+  machineAns = [];
+  computerChoice.textContent = "";
+  ready.textContent = "";
+};
+
+const disableBtns = function () {
+  btns.forEach((btn) => (btn.disabled = true));
+};
+
+const enableBtns = function () {
+  btns.forEach((btn) => (btn.disabled = false));
+};
+
+const guess = function () {
+  machineChoice = Math.trunc(Math.random() * 3) + 1;
+  ans = options[machineChoice];
+  machineAns.push(ans);
+};
+
+const comp = function () {
+  computerChoice.textContent = "";
+};
+
+const fadeOutbtns = function () {
+  choices.classList.add("loss");
+  btns.forEach((btn) => btn.classList.add("loss"));
+  go.classList.add("loss");
+  computerBoard.classList.add("loss");
+  result.classList.add("loss");
+};
+const fadeInbtns = function () {
+  choices.classList.remove("loss");
+  btns.forEach((btn) => btn.classList.remove("loss"));
+  go.classList.remove("loss");
+  computerBoard.classList.remove("loss");
+  result.classList.remove("loss");
+};
+
+const declareWinner = function () {
+  if (state.computerScore === 20) {
+    disableBtns();
+    go.disabled = true;
+    winner.innerHTML = `<div class="score winner">
+  <h3>Computer Wins </h3>
+  <div class="user-score">
+  <p class="score-number user-score-number">${state.computerScore}</p>
+  <span class="emoji"><img src='blue-joobi-laugh.gif'></span>
+  </div>
+  </div>`;
+    fadeOutbtns();
+    // score.classList.add("winner");
+  } else if (state.playerScore === 20) {
+    disableBtns();
+    winner.innerHTML = `<div class="winner">
+  <h3>You Wins</h3>
+  <div class="user-score">
+  <p class="score-number user-score-number">${state.computerScore}</p>
+  </div>
+  </div>`;
+    fadeOutbtns();
+    // score.classList.add("winner");
+  }
+};
+// go.disabled = true;
+console.log(go);
+
+const check = function () {
+  console.log(userAns);
+  computerChoice.textContent = ans;
+  if (
+    userAns[0] === machineAns[0] &&
+    typeof userAns[0] === typeof machineAns[0]
+  ) {
+    state.playerScore++;
+    userScoreNumber.textContent = state.playerScore;
+  } else if (userAns[0] !== machineAns[0]) {
+    state.computerScore++;
+    computerScoreNumber.textContent = state.computerScore;
+  }
+
+  if (state.playerScore >= 1 && state.computerScore <= 3) {
+    emoji.innerHTML = "";
+  } else if (
+    state.playerScore >= 1 &&
+    state.computerScore > 3 * state.playerScore
+  ) {
+    emoji.innerHTML = `<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTE4a2oxNjhmdTBldW13OG1kOHY4YTl3cGFlNnc2YjUydDNmaDd6dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HKoJmvYy3fUqj2Cvrm/giphy.gif'>`;
+    // console.log("😒😒😒😒😒");
+  } else if (
+    state.playerScore >= 1 &&
+    state.computerScore > 2 * state.playerScore
+  ) {
+    emoji.innerHTML = `<img src='blue-joobi-laugh.gif'>`;
+    // console.log("😒😒😒😒😒");
+  }
+  declareWinner();
+};
+
+let btn;
+// const allowClick = function () {
+choices.addEventListener("click", function (e) {
+  btn = e.target;
+  // console.log(btn);
+  btn.classList.contains("choice-btn")
+    ? btn.classList.add("active")
+    : btn.classList.remove("active");
+  userAns.push(`${e.target.textContent}`);
+});
+// };
+// allowClick();
+
+let index = 0;
+const play = function () {
+  if (state.isPlaying) {
+    const timeout = function () {
+      setTimeout(() => {
+        go.disabled = true;
+        ready.textContent = letsGo[0];
+        setTimeout(() => {
+          ready.textContent = letsGo[1];
+          setTimeout(() => {
+            ready.textContent = letsGo[2];
+            setTimeout(() => {
+              ready.textContent = "";
+              disableBtns();
+              // go.disabled = true;
+              setTimeout(() => {
+                comp();
+                check();
+                state.isPlaying = false;
+                setTimeout(() => {
+                  // enableBtns();
+                  disableBtns();
+                  go.disabled = false;
+                  init();
+                }, 1000);
+              }, 650);
+            }, 600);
+          }, 520);
+        }, 510);
+      }, 500);
+    };
+
+    timeout();
+  }
+};
+// play();
+
+go.addEventListener("click", function () {
+  // go.disabled = true;
+  enableBtns();
+  computerChoice.textContent = "";
+  state.isPlaying = true;
+  btns.forEach((btn) => {
+    if (btn.classList.contains("active")) {
+      btn.classList.remove("active");
+    }
+  });
+  init();
+  guess();
+  play();
+  comp();
+  // go.disabled = false;
+  // allowClick();
+});
+
+resetBtn.addEventListener("click", function () {
+  enableBtns();
+  state.computerScore = 0;
+  state.playerScore = 0;
+  init();
+  userScoreNumber.textContent = 0;
+  computerScoreNumber.textContent = 0;
+  btns.forEach((btn) => {
+    if (btn.classList.contains("active")) {
+      btn.classList.remove("active");
+    }
+  });
+  winner.innerHTML = `<div class="score">
+          <h3>Your Score</h3>
+          <div class="user-score">
+            <p class="score-number user-score-number">0</p>
+          </div>
+        </div>
+        <div class="score">
+          <h3>Computer Score</h3>
+          <div class="computer-score">
+            <p class="score-number computer-score-number">0</p>
+            <span class="emoji"></span>
+          </div>
+        </div>`;
+  // enableBtns();
+  fadeInbtns();
+});
